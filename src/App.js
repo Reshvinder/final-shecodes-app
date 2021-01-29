7c083f1584451bde493032061c11bde0f5c4b4ff
Reshvinder <77840545+Reshvinder@users.noreply.github.com>
2021-01-29 15:35:26 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1> Weather App </h1>
+      <btn class="btn btn-info"> submit </btn>
+</div>    
     </div>
   );
 }
